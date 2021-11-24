ffa2c731dbc7a0f7a544dd7ab9de37aa52d1338c
ismaelgomez1986 <ismaelgomez1986@gmail.com>
2021-11-23 22:11:57 -0300

    another commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-publicPath: '/My-Store-Examen-Vuejs'
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/My-Store-Examen-Vuejs/' :
+        '/'
 }
