9e70a4113988e1e5afb7cfb6afd333e339f147b6
ismaelgomez1986 <ismaelgomez1986@gmail.com>
2021-11-23 21:18:51 -0300

    commit before gh-pages

--- src/components/Carrito/Producto.vue
+++ src/components/Carrito/Producto.vue
@@ -1,94 +1,100 @@
 <template>
-<div class="card">
+  <div class="card">
     <div class="row">
-        <div class="col-md-4">
-            <img :src="producto.imagen" class="carrito-imagen" />
-        </div>
-        <div class="col-md-8 px-3">
-            <div class="card-block px-2">
-                <h3 class="card-title">{{ producto.nombre }}</h3>
-                <p class="card-text">Precio: ${{ producto.precio }}</p>
-                <div class="row align-items-center">
-                    <p class="card-text mb-0 mr-4">Cantidad: {{ producto.cantidad }}</p>
-                    <div class="btn-group-vertical">
-                        <button class="btn btn-info" @click="$store.dispatch('carrito/agregarProducto', producto)">
-                            +
-                        </button>
-                        <button class="btn btn-info" @click="$store.dispatch('carrito/quitarProducto', producto)">
-                            -
-                        </button>
-                    </div>
-                </div>
+      <div class="col-md-4">
+        <img :src="producto.imagen" class="carrito-imagen" />
+      </div>
+      <div class="col-md-8 px-3">
+        <div class="card-block px-2">
+          <h3 class="card-title">{{ producto.nombre }}</h3>
+          <p class="card-text">Precio: ${{ producto.precio }}</p>
+          <div class="row align-items-center">
+            <p class="card-text mb-0 mr-4">Cantidad: {{ producto.cantidad }}</p>
+            <div class="btn-group-vertical">
+              <button
+                class="btn btn-info"
+                @click="$store.dispatch('carrito/agregarProducto', producto)"
+              >
+                +
+              </button>
+              <button
+                class="btn btn-info"
+                @click="$store.dispatch('carrito/quitarProducto', producto)"
+              >
+                -
+              </button>
+            </div>
+          </div>
 
-                <p class="card-text">
-                    Subtotal: ${{ (producto.cantidad * producto.precio).toLocaleString('de-DE') }}
-                </p>
-                <p class="card-text">
-                    Total: ${{
+          <p class="card-text">
+            Subtotal: ${{ (producto.cantidad * producto.precio).toLocaleString('de-DE') }}
+          </p>
+          <p class="card-text">
+            Total: ${{
               parseInt(
                 producto.precio * (1 - producto.descuento / 100) * producto.cantidad
               ).toLocaleString('de-DE')
             }}
-                </p>
-                <!-- <a href="#" class="btn btn-primary">Read More</a> -->
-            </div>
+          </p>
+          <!-- <a href="#" class="btn btn-primary">Read More</a> -->
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        producto: {
-            type: Object,
-            required: true
-        }
+  props: {
+    producto: {
+      type: Object,
+      required: true
     }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .card * {
-    text-align: right;
-    font-size: 1rem;
-    font-weight: 500;
+  text-align: right;
+  font-size: 1rem;
+  font-weight: 500;
 }
 
 .btn-group-vertical {
-    padding-left: 20%;
-    margin-bottom: 30px;
+  padding-left: 20%;
+  margin-bottom: 30px;
 }
 
 .card {
-    height: 33vh;
-    background-color: rgb(141, 151, 122);
-    margin: 0 20px;
-    display: inline-block;
-    border-radius: 20px;
-    border-style: none;
-    box-shadow: 13px 13px 5px rgb(82, 82, 1);
+  height: 33vh;
+  background-color: rgb(141, 151, 122);
+  margin: 0 20px;
+  display: inline-block;
+  border-radius: 20px;
+  border-style: none;
+  box-shadow: 13px 13px 5px rgb(82, 82, 1);
 
-    &:hover {
-        box-shadow: 13px 13px 5px rgb(42, 42, 0);
-        transform: scale(1.1);
-        transition: .5s;
-    }
+  &:hover {
+    box-shadow: 13px 13px 5px rgb(42, 42, 0);
+    transform: scale(1.1);
+    transition: 0.5s;
+  }
 
-    &:not( :hover) {
-        transform: scale(1);
-        transition: .5s;
-    }
+  &:not(:hover) {
+    transform: scale(1);
+    transition: 0.5s;
+  }
 }
 
 .carrito-imagen {
-    width: 130px;
-    height: 100px;
-    border-top-left-radius: 18px;
+  width: 130px;
+  height: 100px;
+  border-top-left-radius: 18px;
 }
 
 .card-title {
-    font-size: 1.5rem;
-    font-weight: 600;
+  font-size: 1.5rem;
+  font-weight: 600;
 }
 </style>
--- src/components/Productos/BuscarProductos.vue
+++ src/components/Productos/BuscarProductos.vue
@@ -1,103 +1,109 @@
 /* eslint-disable prettier/prettier */
 <template>
-<div>
+  <div>
     <h1>Todos los productos</h1>
     <hr />
     <div class="header">
-        <h3>Buscar Productos</h3>
-        <input type="text" v-model="producto" placeholder="Ingrese un nombre de producto, precio máximo o categoría" />
+      <h3>Buscar Productos</h3>
+      <input
+        type="text"
+        v-model="producto"
+        placeholder="Ingrese un nombre de producto, precio máximo o categoría"
+      />
     </div>
 
     <div class="row">
-        <div class="col-sm mb-4 mb-md-0" v-for="(item, $index) in productoDisponiblePorNombre(producto.toLowerCase())" :key="$index">
-            <TarjetaProducto :producto="item" />
-            <br />
-        </div>
+      <div
+        class="col-sm mb-4 mb-md-0"
+        v-for="(item, $index) in productoDisponiblePorNombre(producto.toLowerCase())"
+        :key="$index"
+      >
+        <TarjetaProducto :producto="item" />
+        <br />
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 import TarjetaProducto from './TarjetaProducto.vue'
-import {
-    mapGetters
-} from 'vuex'
+import { mapGetters } from 'vuex'
 export default {
-    name: 'buscar',
-    data() {
-        return {
-            producto: ''
-        }
-    },
-    components: {
-        TarjetaProducto
-    },
+  name: 'buscar',
+  data() {
+    return {
+      producto: ''
+    }
+  },
+  components: {
+    TarjetaProducto
+  },
 
-    computed: mapGetters('productos', ['productoDisponiblePorNombre'])
+  computed: mapGetters('productos', ['productoDisponiblePorNombre'])
 }
 </script>
 
 <style lang="scss" scoped>
 h3,
 input {
-    display: inline-block;
-    margin: 20px 1.5vw;
-    border-radius: 5px;
+  display: inline-block;
+  margin: 20px 1.5vw;
+  border-radius: 5px;
 }
 
 h1 {
-    font-size: 3rem;
-    font-weight: 700;
+  font-size: 3rem;
+  font-weight: 700;
 }
 
 h3 {
-    font-size: 2rem;
+  font-size: 2rem;
 }
 
 input {
-    width: 57vw;
-    height: 40px;
-    background-color: rgb(158, 168, 136);
+  width: 57vw;
+  height: 40px;
+  background-color: rgb(158, 168, 136);
 
-    @media (max-width: 1005px) {
-        width: 70vw;
-    }
+  @media (max-width: 1005px) {
+    width: 70vw;
+  }
 }
 
 ::placeholder {
-    padding-left: 15px;
-    color: rgb(59, 57, 57);
-    font-size: 1.3rem;
+  padding-left: 15px;
+  color: rgb(59, 57, 57);
+  font-size: 1.3rem;
 
-    @media (max-width: 1024px) {
-        font-size: 1.1rem;
-    }
+  @media (max-width: 1024px) {
+    font-size: 1.1rem;
+  }
 }
 
 .header {
-    margin: 0 0 50px 3vw;
-    width: 88vw;
-    background-color: olive;
-    border-radius: 10px;
-    box-shadow: 13px 13px 5px rgb(82, 82, 1);
+  margin: 0 0 50px 3vw;
+  width: 88vw;
+  background-color: olive;
+  border-radius: 10px;
+  box-shadow: 13px 13px 5px rgb(82, 82, 1);
 
-    &:hover {
-        size: 105%;
-        box-shadow: 13px 13px 5px rgb(42, 42, 0);
-        transition: .5s;
-    }
+  &:hover {
+    size: 105%;
+    box-shadow: 13px 13px 5px rgb(42, 42, 0);
+    transition: 0.5s;
+  }
 
-    @media (max-width: 1005px) {
-        width: 80vw;
-    }
+  @media (max-width: 1005px) {
+    width: 80vw;
+  }
 }
 
 .row {
-    text-align: center;
+  text-align: center;
 }
 
 hr {
-    border: 2px solid rgb(49, 71, 3);
-    background-color:  rgb(49, 71, 3);
+  border: 2px solid rgb(49, 71, 3);
+  background-color: rgb(49, 71, 3);
 }
 </style>
